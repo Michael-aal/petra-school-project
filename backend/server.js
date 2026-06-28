ff37c6817ea90a2ae75647b94d3a4bde1d3fc490
OlatomiwaTech <mrcodex2012@gmail.com>
2026-06-28 12:38:52 +0100

    Changes from v2.0

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,29 @@
 const express = require('express');
 import {config} from "dotenv";
+import { connectDB, disconnectDB} from "./config/db.js";
 
 const app = express();
 config();
+connectDB();
+
+process.on("unhandledRejection", (err) => {
+    console.log('Unhandled Rejection :', err)
+    server.close(async () => {  
+        await disconnectDB();
+        process.exit(1);
+    })
+});
+process.on("uncaughtException", (err) => {
+    console.error("Uncaught Exception : ", err)
+    await disconnectDB();
+    process.exit(1);
+})
+process.on("SIGTERM", async () => {
+    console.log("SIGTERM, Shutting Down Gracefully")
+    server.close(async () => {
+         await disconnectDB();
+         process.exit(0);
+    });
+
+});
 
