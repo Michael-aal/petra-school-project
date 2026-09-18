3ccded796793934682867903669759a3acb523cd
SATOSHI <augustinemichael8910@gmail.com>
2026-09-18 16:02:18 +0100

    feat(ui): apply Petra professional theme globally

--- Petra-Project/src/main.jsx
+++ Petra-Project/src/main.jsx
@@ -4,6 +4,7 @@ import { UserProvider } from "./context/UserContext.jsx";
 import { SchoolProvider } from "./context/SchoolContext.jsx";
 import { ToastProvider, ToastViewport } from "./context/ToastContext.jsx";
 import "./index.css";
+import "./Styles/petra-theme.css";
 import App from "./App.jsx";
 import { applyTheme } from "./utils/theme.js";
 
