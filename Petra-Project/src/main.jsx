87b79674e0dc215e94bb309f149edb03ee5a5237
SATOSHI <augustinemichael8910@gmail.com>
2026-09-18 16:04:34 +0100

    feat(ui): enable shared responsive hardening

--- Petra-Project/src/main.jsx
+++ Petra-Project/src/main.jsx
@@ -5,6 +5,7 @@ import { SchoolProvider } from "./context/SchoolContext.jsx";
 import { ToastProvider, ToastViewport } from "./context/ToastContext.jsx";
 import "./index.css";
 import "./Styles/petra-theme.css";
+import "./Styles/petra-responsive.css";
 import App from "./App.jsx";
 import { applyTheme } from "./utils/theme.js";
 
