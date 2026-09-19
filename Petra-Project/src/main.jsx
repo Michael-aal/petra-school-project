632fe555b45d6653252b931766def5c664004e26
SATOSHI <augustinemichael8910@gmail.com>
2026-09-19 08:33:12 +0100

    feat(ui): apply Nuvora minimal design layer globally

--- Petra-Project/src/main.jsx
+++ Petra-Project/src/main.jsx
@@ -7,6 +7,7 @@ import "./index.css";
 import "./Styles/petra-theme.css";
 import "./Styles/petra-responsive.css";
 import "./Styles/nuvora-theme.css";
+import "./Styles/nuvora-minimal.css";
 import App from "./App.jsx";
 import { applyTheme } from "./utils/theme.js";
 applyTheme();
